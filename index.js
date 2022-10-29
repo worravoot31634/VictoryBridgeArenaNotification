5851d0fa5f114ddc634b22f9861e048dd9a6a440
oat.wrwoot <55147927+worravoot31634@users.noreply.github.com>
2022-10-30 02:24:30 +0700

    initial commit

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// GuildID 1035643754251567136
-
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
