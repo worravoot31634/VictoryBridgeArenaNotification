eb59c413f75e4d9dc43d6d1c51b430b57e5f690c
oat.wrwoot <55147927+worravoot31634@users.noreply.github.com>
2022-11-13 03:31:55 +0700

    add Procfile

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+var delayInMilliseconds = 5000; //5 second
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   schedule.scheduleJob("0 0 * * *", () => {
+    console.log("Update arena time from scedule");
     // channal that notify the [Arena time]
-
-    var arenaChannel = client.channels.cache.get("1035961848706646116");
-    arenaChannel.setName(`[10-10]: ${victoryBrideTime}`);
+    setTimeout(function () {
+      var arenaChannel = client.channels.cache.get("1035961848706646116");
+      arenaChannel.setName(`[10-10]: ${victoryBrideTime}`);
+    }, delayInMilliseconds);
   });
 });
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "ping") {
+  if (interaction.commandName === "arena") {
     await interaction.reply(`[10-10]: ${victoryBrideTime}`);
 
     interaction.guild.channels
